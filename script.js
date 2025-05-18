9287d7b7c4b189dc434f2df3967369e3e1ee347d
GAZAL2025 <ghaazaallll2025@gmail.com>
2025-05-18 21:06:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -127,13 +127,16 @@ function removeFromCart(id) {
 }
 
 
-document.addEventListener("DOMContentLoaded", function () {
-  const menuButton = document.querySelector(".menu-toggle");
-  const navMenu = document.getElementById("navMenu");
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
 
-  if (menuButton && navMenu) {
-    menuButton.addEventListener("click", function () {
-      navMenu.classList.toggle("show");
-    });
-  }
-});
+const mobileMenu = () => {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+}
+
+menu.addEventListener('click', mobileMenu);
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Hello World";
+}
