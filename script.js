5d7a7435c79f4982cba55cb411d60e0b0dcf7067
GAZAL2025 <ghaazaallll2025@gmail.com>
2025-05-19 17:21:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -133,7 +133,7 @@ function removeFromCart(id) {
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 
-menu?.addEventListener('click', () => {
+menu.addEventListener('click', () => {
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
 });
